3207426b63940715566443d088a3efbcd95d6544
user <ilasmyga26@gmail.com>
2023-11-30 00:35:51 +0300

    ...

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,5 +1,24 @@
 public class Ex3 {
     public static void main (String [] args){
+        /* создать n переменных char всеми возможными способами и вывести их на экран*/
 
+        char zahl = 100;
+        // Сотый по счёту символ в таблице
+        char octal = '\u039A';
+        // Вызов определённого символа кодом
+        char zeichen = 'A';
+        // Буква A
+        char tabulator = '\t'; // В русском языке клавиша называется "Таб"
+        // Табулятор
+        char phi = '\u03A6';
+        // Греческая буквая Фита - PHI
+
+
+        System.out.println(zahl);
+        System.out.println(octal);
+        System.out.println(zeichen);
+        System.out.print(tabulator);
+        System.out.println(phi);
     }
 }
+
